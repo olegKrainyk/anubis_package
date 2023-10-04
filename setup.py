85bc09d54837a902da0f3aa3c990a1e7c6536e02
olegKrainyk <oleg.kr944@gmail.com>
2023-10-04 13:06:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='anubis',
-    version='0.0.1',
+    version='0.0.2',
     author='Oleh Krainyk',
     author_email='oleh@cuttingedgeai.com',
     description='Location calculator',
